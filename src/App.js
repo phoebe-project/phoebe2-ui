a9b8ce4e2f46c63509136fe92f49754ca777404a
Kyle Conroy <kyleconroy@gmail.com>
2018-12-09 14:31:33 -0500

    detect whether running as electron or web-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import isElectron from 'is-electron'; // https://github.com/cheton/is-electron
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,11 @@ class App extends Component {
         <p className="App-intro">
           Welcome to PHOEBE User Interface!
         </p>
+        { isElectron() ?
+          <p>Running from within Electron</p>
+          :
+          <p>Running as a web-app</p>
+        }
       </div>
     );
   }
