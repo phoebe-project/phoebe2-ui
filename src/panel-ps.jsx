phoebe-project/phoebe2-ui

commit ce94d3dc7f4cd1f5435869be0b67fe451743ea17

display RunChecksReport in UI
and account for multiple instances of the same parameter in the middle panel

src/panel-ps.jsx
@@ -79,7 +79,7 @@ class Parameter extends Component {
     this.expandFromClick = false;
   }
   toggleExpandedDetails = () => {
-    if (this.props.PSPanel.state.activeParameter===this.props.uniqueid) {
+    if (this.props.PSPanel.state.activeParameter===this.props.uniqueidkey) {
       if (this.props.PSPanel.state.activeParameterDetails) {
         this.props.PSPanel.setState({activeParameter: null})
         this.props.PSPanel.setState({activeParameterDetails: false})
@@ -91,7 +91,7 @@ class Parameter extends Component {
     } else {
       // try to prevent the scroll action
       this.expandFromClick = true;
-      this.props.PSPanel.setState({activeParameter: this.props.uniqueid})
+      this.props.PSPanel.setState({activeParameter: this.props.uniqueidkey})
       this.props.PSPanel.setState({activeParameterDetails: true})
       this.props.PSPanel.setState({activeParameterValue: false})
       this.props.PSPanel.setState({activeParameterUnit: false})
@@ -99,7 +99,7 @@ class Parameter extends Component {
   }
   toggleExpandedValue = (e) => {
     e.stopPropagation();
-    if (this.props.PSPanel.state.activeParameter===this.props.uniqueid) {
+    if (this.props.PSPanel.state.activeParameter===this.props.uniqueidkey) {
       // this.props.PSPanel.setState({activeParameter: null})
       // this.props.PSPanel.setState({activeParameterDetails: false})
       this.props.PSPanel.setState({activeParameterValue: !this.props.PSPanel.state.activeParameterValue})
@@ -107,7 +107,7 @@ class Parameter extends Component {
     } else {
       // try to prevent the scroll action
       this.expandFromClick = true;
-      this.props.PSPanel.setState({activeParameter: this.props.uniqueid})
+      this.props.PSPanel.setState({activeParameter: this.props.uniqueidkey})
       this.props.PSPanel.setState({activeParameterDetails: false})
       this.props.PSPanel.setState({activeParameterValue: true})
       this.props.PSPanel.setState({activeParameterUnit: false})
@@ -131,7 +131,7 @@ class Parameter extends Component {
   }
   toggleExpandedUnit = (e) => {
     e.stopPropagation();
-    if (this.props.PSPanel.state.activeParameter===this.props.uniqueid) {
+    if (this.props.PSPanel.state.activeParameter===this.props.uniqueidkey) {
       // this.props.PSPanel.setState({activeParameter: null})
       // this.props.PSPanel.setState({activeParameterDetails: false})
       this.props.PSPanel.setState({activeParameterValue: false})
@@ -139,7 +139,7 @@ class Parameter extends Component {
     } else {
       // try to prevent the scroll action
       this.expandFromClick = true;
-      this.props.PSPanel.setState({activeParameter: this.props.uniqueid})
+      this.props.PSPanel.setState({activeParameter: this.props.uniqueidkey})
       this.props.PSPanel.setState({activeParameterDetails: false})
       this.props.PSPanel.setState({activeParameterValue: false})
       this.props.PSPanel.setState({activeParameterUnit: true})
@@ -186,7 +186,7 @@ class Parameter extends Component {
       this.setState({pinned: ispinned})
     }
 
-    var active = this.props.PSPanel.state.activeParameter===this.props.uniqueid || this.props.bundle.state.paramsfilteredids.length===1;
+    var active = this.props.PSPanel.state.activeParameter===this.props.uniqueidkey || this.props.bundle.state.paramsfilteredids.length===1;
     var expandedDetails = active && this.props.PSPanel.state.activeParameterDetails
     if (expandedDetails !== this.state.expandedDetails) {
       this.setState({expandedDetails: expandedDetails})
@@ -495,7 +495,7 @@ class ParameterDetailsItemPin extends Component {
     this.props.PSPanel.setState({activeParameterDetails: true})
     this.props.PSPanel.setState({activeParameterValue: false})
     this.props.PSPanel.setState({activeParameterUnit: false})
-    this.props.PSPanel.setState({activeParameter: this.props.uniqueid})
+    this.props.PSPanel.setState({activeParameter: this.props.uniqueidkey})
   }
   popParameter = () => {
     var bundleid = this.props.bundle.state.bundleid || this.props.bundle.match.params.bundleid
@@ -761,6 +761,33 @@ class InputConstraint extends Component {
   }
 }
 
+class ChecksReportItem extends Component {
+  render() {
+
+    var style = {padding: "10px", marginBottom: "5px", borderLeft: "4px solid"}
+
+    if (this.props.report.level == 'ERROR') {
+      style.borderColor = '#ff000040';
+    } else if (this.props.report.level == 'WARNING') {
+      style.borderColor = '#efff0099';
+    }
+
+
+    return (
+      <div style={style}>
+        <b>{this.props.report.level}</b>: {this.props.report.message}
+
+        {Object.keys(this.props.report.parameters).map(twig => {
+          let uniqueid = this.props.report.parameters[twig]
+          let param = this.props.bundle.state.params[uniqueid]
+          return <Parameter key={uniqueid} uniqueid={uniqueid} uniqueidkey={"checks:"+this.props.reportKey+":"+uniqueid} app={this.props.app} bundle={this.props.bundle} PSPanel={this.props.PSPanel} paramOverview={param} pinnable={true} disableFiltering={false} disableScrollTo={true} description={param.description}/>
+        })}
+
+      </div>
+    )
+  }
+}
+
 export class PSPanel extends Component {
   constructor(props) {
     super(props);
@@ -818,6 +845,25 @@ export class PSPanel extends Component {
 
     return (
       <Panel backgroundColor="#e4e4e4" minHeight={this.props.minHeight}>
+
+        {this.props.showChecks ?
+          <div className="phoebe-parameter" style={{padding: "10px"}}>
+            {this.props.bundle.state.checksReport.length == 0 ?
+              <span style={{borderLeft: "4px solid #04d804b8", padding: "10px", marginBottom: "5px"}}><b>PASSING</b>: no errors or warnings to show</span>
+              :
+              null
+            }
+
+            {this.props.bundle.state.checksReport.map((report,i) => {
+              return <ChecksReportItem report={report} reportKey={i} bundle={this.props.bundle} PSPanel={this} app={this.props.app}/>
+            })}
+          </div>
+          :
+          null
+        }
+
+
+
         {this.props.showPopoutButton ?
           <div style={{float: "right", marginTop: "6px", paddingRight: "10px"}}>
             <span className="btn btn-blue" onClick={this.popPS} style={{height: "34px", width: "34px"}} title="popout into external window"><span className="fas fa-fw fa-external-link-alt"/></span>
@@ -867,7 +913,7 @@ class PSGroup extends Component {
     var parameters = []
     parameters = mapObject(this.props.paramsFiltered, (uniqueid, param) => {
       if (param[this.props.orderBy]===this.props.orderByTag) {
-        return (<Parameter key={uniqueid} app={this.props.app} bundle={this.props.bundle} PSPanel={this.props.PSPanel} paramOverview={param} uniqueid={uniqueid} pinnable={!this.props.PSPanelOnly} disableFiltering={this.props.disableFiltering} description={param.description}/>)
+        return (<Parameter key={uniqueid} uniqueid={uniqueid} uniqueidkey={'PS:'+uniqueid} app={this.props.app} bundle={this.props.bundle} PSPanel={this.props.PSPanel} paramOverview={param} pinnable={!this.props.PSPanelOnly} disableFiltering={this.props.disableFiltering} description={param.description}/>)
       }
     })
 

src/bundle.jsx
@@ -29,6 +29,8 @@ export class Bundle extends ReactQueryParams {
       bundleid: props.match.params.bundleid,
       params: null,
       failedConstraints: [],
+      checksReport: [],
+      checksStatus: "UNKNOWN",
       paramsfilteredids: [],
       tags: {},
       tagsAvailable: {},
@@ -78,6 +80,10 @@ export class Bundle extends ReactQueryParams {
 
       }
 
+    })
+
+    this.props.app.socket.on(this.state.bundleid+':checks:react', (data) => {
+      this.setState({checksReport: data.checks_report || [], checksStatus: data.checks_status || "UNKNOWN"})
     })
 
     this.props.app.socket.on(this.state.bundleid+':failed_constraints:react', (data) => {
@@ -164,7 +170,7 @@ export class Bundle extends ReactQueryParams {
       .then(json => {
         if (json.data.success) {
           this.registerBundle();
-          this.setState({params: json.data.parameters, tags: json.data.tags, failedConstraints: json.data.failed_constraints, nparams: Object.keys(json.data.parameters).length})
+          this.setState({params: json.data.parameters, tags: json.data.tags, failedConstraints: json.data.failed_constraints, checksStatus: json.data.checks_status || "UNKNOWN", checksReport: json.data.checks_report || [], nparams: Object.keys(json.data.parameters).length})
         } else {
           alert("server error: "+json.data.error);
           this.setState({params: null, tags: null});
@@ -219,7 +225,7 @@ export class Bundle extends ReactQueryParams {
     return inAdvanced
   }
   filter = (params, filter, ignoreGroups=[]) => {
-    var ignoreGroupsFilter = ignoreGroups.concat(["pinned", "advanced", "orderBy", "tmp"])
+    var ignoreGroupsFilter = ignoreGroups.concat(["pinned", "advanced", "orderBy", "tmp", "checks"])
 
     var nAdvancedHiddenEach = {};
     var nAdvancedHiddenTotal = 0;
@@ -385,7 +391,7 @@ export class Bundle extends ReactQueryParams {
             {this.props.match.params.action ?
               <ActionPanel app={this.props.app} bundleid={this.state.bundleid} bundle={this} action={this.props.match.params.action}/>
               :
-              <PSPanel app={this.props.app} bundleid={this.state.bundleid} bundle={this} showPopoutButton={true}/>
+              <PSPanel app={this.props.app} bundleid={this.state.bundleid} bundle={this} showPopoutButton={true} showChecks={!this.queryParams.hideChecks}/>
             }
             <FigurePanel app={this.props.app} bundleid={this.state.bundleid} bundle={this} inactive={this.props.match.params.action}/>
           </PanelGroup>

src/panel-tags.jsx
@@ -429,6 +429,69 @@ class FilterBox extends Component {
   }
 }
 
+class ChecksBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+  toggleExpanded = () => {
+    this.setState({expanded: !this.state.expanded})
+  }
+  // toggleCheckbox = (item, checked) => {
+  //   var currentChecks = this.props.bundle.queryParams.checks || []
+  //   let newChecks
+  //   if (checked) {
+  //     newChecks = currentChecks.concat(item)
+  //   } else {
+  //     newChecks = currentChecks.filter(v => v !== item)
+  //   }
+  //   this.props.bundle.setQueryParams({checks: newChecks})
+  // }
+  // toggleErrors = (e) => {
+  //   this.toggleCheckbox("errors", e.currentTarget.checked)
+  // }
+  // toggleWarnings = (e) => {
+  //   this.toggleCheckbox("warnings", e.currentTarget.checked)
+  // }
+  // toggleConstraints = (e) => {
+  //   this.toggleCheckbox("constraints", e.currentTarget.checked)
+  // }
+  toggleViewMessages = (e) => {
+    var currentChecks = this.props.bundle.queryParams.hideChecks || false
+    this.props.bundle.setQueryParams({hideChecks: !currentChecks})
+  }
+  render() {
+    var status = this.props.bundle.state.checksStatus
+
+    var hidingChecks = this.props.bundle.queryParams.hideChecks || false
+
+    var viewingChecksToggleTitle = 'hide messages'
+    if (hidingChecks) {
+      viewingChecksToggleTitle = 'view messages'
+    }
+
+
+    var style = {padding: "10px"}
+
+    if (status == 'FAIL') {
+      style.backgroundColor = '#ff000040';
+    } else if (status == 'WARNING') {
+      style.backgroundColor = '#efff0099';
+    }
+
+    return (
+      <div className="phoebe-parameter" style={style}>
+        Checks Status: {status}
+        {/* <span style={{float: "right", color: "#2B71B1", cursor: "pointer"}} onClick={this.toggleExpanded}>{this.state.expanded ? "hide options" : "show options"}</span> */}
+        <span style={{float: "right", width: "50%", maxWidth: "300px", minWidth: "100px"}} className="btn btn-tag btn-tag-clear" onClick={this.toggleViewMessages}>{viewingChecksToggleTitle}</span>
+
+      </div>
+    )
+  }
+}
+
 export class TagPanel extends Component {
   render() {
     var tags = this.props.bundle.state.tags || {}
@@ -436,6 +499,7 @@ export class TagPanel extends Component {
     return (
       <Panel inactive={this.props.inactive}>
         <FilterBox bundle={this.props.bundle}/>
+        <ChecksBox bundle={this.props.bundle}/>
 
         <TagGroup title="Context" app={this.props.app} bundle={this.props.bundle} bundleid={this.props.bundleid} expanded={true} tags={tags.contexts || null}/>
         <TagGroup title="Kind" app={this.props.app} bundle={this.props.bundle} bundleid={this.props.bundleid} tags={tags.kinds || null}></TagGroup>

commit b9c6f0b3f78e8d08f7ac91c1b9dbba846cae127d

fix go-to pinned parameter
broken by ce94d3dc7f4cd1f5435869be0b67fe451743ea17 when allowing for multiple instances of the same parameter

src/panel-ps.jsx
@@ -495,7 +495,7 @@ class ParameterDetailsItemPin extends Component {
     this.props.PSPanel.setState({activeParameterDetails: true})
     this.props.PSPanel.setState({activeParameterValue: false})
     this.props.PSPanel.setState({activeParameterUnit: false})
-    this.props.PSPanel.setState({activeParameter: this.props.uniqueidkey})
+    this.props.PSPanel.setState({activeParameter: 'PS:'+this.props.uniqueid})
   }
   popParameter = () => {
     var bundleid = this.props.bundle.state.bundleid || this.props.bundle.match.params.bundleid