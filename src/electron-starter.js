0b71f14069c961b85384eb8564df403766c2ceee
Kyle <kyleconroy@gmail.com>
2020-07-17 11:18:32 -0400

    fix compatibility issues with update to electron 9

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -94,6 +94,7 @@ const BrowserWindow = electron.BrowserWindow;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+var showExitPrompt = true;
 
 function createWindow() {
     // Create the browser window.
@@ -104,7 +105,7 @@ function createWindow() {
       height: 800,
       minHeight: 500,
       icon: __dirname + '/icons/phoebe.png',
-      // webPreferences: { nodeIntegration: true }
+      webPreferences: { nodeIntegration: true }
     });
 
     // and load the index.html of the app.
@@ -118,9 +119,8 @@ function createWindow() {
     // mainWindow.webContents.openDevTools();
 
     // Confirm before closing the app as that will kill the child-process server
-    mainWindow.showExitPrompt = true;
     mainWindow.on('close', function(e) {
-      if (mainWindow.showExitPrompt) {
+      if (showExitPrompt) {
         e.preventDefault();
 
         if (global.pyPort) {
@@ -131,7 +131,7 @@ function createWindow() {
               var connectedClients = json.data.clients.filter(client => client !== global.clientid)
               if (connectedClients.length > 0 && !options.argv.c) {
                 // TODO: only show this if there are clients connected to the server.  Will need to have all clients subscribe and then have the server return the clientids in this fetch.
-                choice = electron.dialog.showMessageBox(
+                choice = electron.dialog.showMessageBoxSync(
                   {
                       type: 'question',
                       buttons: ['Quit', 'Cancel'],
@@ -140,18 +140,17 @@ function createWindow() {
                   });
 
               }
-
               if (choice===0) {
-                mainWindow.showExitPrompt = false;
+                showExitPrompt = false;
                 mainWindow.close();
               }
             })
             .catch(err => {
-              mainWindow.showExitPrompt = false;
+              showExitPrompt = false;
               mainWindow.close();
             });
         } else {
-          mainWindow.showExitPrompt = false;
+          showExitPrompt = false;
           mainWindow.close();
         }
       }
